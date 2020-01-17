1c040a8ccb6a88bc3fa8d8a874e806d77f644034
Trent1048 <trentbultsma@gmail.com>
2020-01-16 23:10:14 -0800

    add bear

---
+++ Bear.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+
+public class Bear extends Critter {
+
+    private boolean isPolar;
+    private boolean pointLeft = true; // Weather or not the slash should point left or right
+
+    public Bear(boolean polar) {
+        isPolar = polar;
+    }
+
+    // always infect if an enemy is in front
+    // otherwise hop if possible
+    // otherwise turn left
+    public Action getMove(CritterInfo info) {
+        if (info.getFront() == Neighbor.OTHER) return Action.INFECT;
+        else if (info.getFront() == Neighbor.EMPTY) return Action.HOP;
+        return Action.LEFT;
+    }
+
+    // white for a polar bear
+    // black otherwise
+    public Color getColor() {
+        if (isPolar) return Color.WHITE;
+        return Color.BLACK;
+    }
+
+    // alternate on each different move between a slash character and a backslash character
+    public String toString() {
+        // updates the direction for the next turn
+        pointLeft = !pointLeft;
+
+        if (pointLeft) return "\\";
+        return "/";
+    }
+}
---
+++ out/production/Critters/README.md
@@ -0,0 +1,2 @@
+# Critters
+Plays a game with lots of different critters that go around and infect each other.
