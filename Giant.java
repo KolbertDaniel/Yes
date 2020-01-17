8c9841cc653742d7d727cbbd9a3037a4c2a2cb7f
Derulan <jpmpowers@gmail.com>
2020-01-17 14:05:25 -0800

    Add giant. He yeets

---
+++ Giant.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+
+public class Giant extends Critter{
+
+    private static String[] modes = new String[] {"fee","fie","foe","fum"};
+    private int mode = 0;
+
+    public Action getMove(CritterInfo info) {
+        mode = (mode+1)%24;
+        Neighbor front = info.getFront();
+
+        if (front == Neighbor.OTHER) return Action.INFECT;
+        else if (front == Neighbor.EMPTY) return Action.HOP;
+        return Action.RIGHT;
+    }
+
+    // Giant is grey
+    public Color getColor() {
+        return Color.GRAY;
+    }
+
+    // alternate appearance depending on the mode of the current
+    public String toString() {
+        return modes[mode/6];
+    }
+}
